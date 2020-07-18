58663b4f8664d8eaa60009103b5612b531965a8f
RagAndCodeMan <idmitriywinx@gmail.com>
2020-07-18 07:45:36 +0300

    Added responses for GET requests

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const socket        = io(HTTPServer)            // initialize socket.io
 
 const PORT = process.env.PORT || 3000 
 
-// including middleware handlers
+//#region including middleware handlers
 app.set("view engine", "ejs")
 
 app.use(cookie())
@@ -19,18 +19,39 @@ app.use(HTTPRequestParser.urlencoded({
     type: "application/x-www-form-urlencoded",
     extended: true
 }))
+//#endregion
 
-// getting favicon
-app.get("/favicon.ico", (req, res) => {
-    res.sendFile(path.join(__dirname, "/favicon.ico"))
+//#region include static files
+app.get("/static/favicon.ico", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/favicon.ico")) // getting favicon
 })
 
-// handling requests
-app.get('/', (req, res) => {
-    res.render("index", {
-        dataGovno: "jopa"
-    })
+app.get("/static/lib/bootstrap/bootstrap.min.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/lib/bootstrap.min.css")) // getting bootsrap css
 })
+
+app.get("/static/lib/bootstrap/bootstrap.min.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/lib/bootstrap.min.js")) // getting bootsrap js
+})
+
+app.get("/static/site.webmanifest", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/site.webmanifest")) // getting webmanifest
+})
+
+app.get("/static/favicon-32x32.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/favicon-32x32.png")) // getting favicon
+})
+
+app.get("/static/favicon-16x16.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "/static/favicon-16x16.png")) // getting favicon
+})
+//#endregion
+
+//#region handling client requests
+app.get("/", (req, res) => {
+    res.render('index')
+})
+//#endregion 
 
 // starting server
 HTTPServer.listen(PORT, () => {
